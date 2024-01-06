b8f410178fcdd788c571ac601045daf1bc80513f
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-01-06 19:58:00 +0100

    added server

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+// Set static folder
+app.use(express.static('public'));
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true}));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// Start the server
+listen(3000, () => {
+console.log('Server listening successfully on port 3000')
+});
